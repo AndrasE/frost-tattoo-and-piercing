38beac5d710287bf747b8270cfc0aa60e84ef209
Andras E <rohadtsajt@gmail.com>
2024-06-03 19:34:19 +0100

    navbar design finish
    w/ dropdown

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,6 +8,8 @@ import {
   NavMenu,
   NavItem,
   NavLinks,
+  NavBtn,
+  NavBtnLink
 } from "./NavbarElements";
 
 function Navbar() {
@@ -17,13 +19,25 @@ function Navbar() {
         <NavbarContainer>
           <NavLogo to="/">Frost</NavLogo>
           <MobileIcon>
-          <FaBars />
+            <FaBars />
           </MobileIcon>
           <NavMenu>
             <NavItem>
               <NavLinks to="about">About</NavLinks>
             </NavItem>
+            <NavItem>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign-up</NavLinks>
+            </NavItem>
           </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="signin">Sign-in</NavBtnLink>
+          </NavBtn>
         </NavbarContainer>
       </Nav>
     </>
