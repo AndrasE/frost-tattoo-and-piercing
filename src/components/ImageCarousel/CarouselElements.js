8387ed89f5629d14302429611714446ad12b7b90
Andras E <rohadtsajt@gmail.com>
2024-06-17 21:03:18 +0100

    carousel part III styling

--- src/components/ImageCarousel/CarouselElements.js
+++ src/components/ImageCarousel/CarouselElements.js
@@ -1,10 +1,27 @@
 import styled from "styled-components";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+
+export const CarouselWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  object-fit: cover;
+  width: 400px;
+  height: 400px;
+`;
 
 export const Img = styled.img`
-  border: 1px solid red;
-  height: 200px;
-  width: 200px;
-  border: 3px solid rebeccapurple;
+  border-radius: 20px;
+  width: 100%;
+  height: 100%;
+`;
+
+export const LeftArrow = styled(BsArrowLeftCircleFill)`
+  color: #fff;
+`;
+
+export const RightArrow = styled(BsArrowRightCircleFill)`
+  color: #fff;
 `;
 
 export const ButtonIndicator = styled.button`
--- src/components/ImageCarousel/index.js
+++ src/components/ImageCarousel/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import carouselData from "./carouselData.json";
-import { Img, ButtonIndicator } from "./CarouselElements";
-import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import {
+  CarouselWrapper,
+  Img,
+  LeftArrow,
+  RightArrow,
+  ButtonIndicator,
+} from "./CarouselElements";
 
 const Carousel = () => {
   return (
     <div>
-      <BsArrowLeftCircleFill />
-      {carouselData.slides.map((item, index) => {
-        return <Img src={item.src} alt={item.alt} key={item.index} />;
-      })}
-      <BsArrowRightCircleFill />
+      <LeftArrow />
+      <CarouselWrapper>
+        {carouselData.slides.map((item, index) => {
+          return <Img src={item.src} alt={item.alt} key={item.index} />;
+        })}
+      </CarouselWrapper>
+      <RightArrow />
       {carouselData.slides.map((_, index) => {
         return <ButtonIndicator key={index} onClick={null} />;
       })}
