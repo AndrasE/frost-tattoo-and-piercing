5beb65cb2cf88d0ef879133c70bf2a3f2dd9a947
Andras E <rohadtsajt@gmail.com>
2024-06-23 13:03:20 +0100

    autoplay lightGallery.js

--- src/pages/GalleryPage/index.js
+++ src/pages/GalleryPage/index.js
@@ -1,17 +1,39 @@
 import React from "react";
 import LightGallery from "lightgallery/react";
+
 import lgThumbnail from "lightgallery/plugins/thumbnail";
 import lgZoom from "lightgallery/plugins/zoom";
 import asd2 from "../../images/galleryPageImgs/images/1.jpg";
 import asd from "../../images/galleryPageImgs/thumbnails/1.jpg";
+import lgAutoplay from "lightgallery/plugins/autoplay";
+
 import "lightgallery/css/lightgallery.css";
 import "lightgallery/css/lg-zoom.css";
 import "lightgallery/css/lg-thumbnail.css";
+import "lightgallery/css/lg-autoplay.css";
 
 const Gallery = () => {
   return (
     <>
-      <LightGallery plugins={[lgZoom]} mode="lg-fade">
+      <LightGallery
+        plugins={[lgZoom, lgAutoplay, lgThumbnail]}
+        mode="lg-fade"
+        lgAuto>
+        <a href={asd2}>
+          <img alt="img1" src={asd} />
+        </a>
+        <a href={asd2}>
+          <img alt="img1" src={asd} />
+        </a>
+        <a href={asd2}>
+          <img alt="img1" src={asd} />
+        </a>
+        <a href={asd2}>
+          <img alt="img1" src={asd} />
+        </a>
+        <a href={asd2}>
+          <img alt="img1" src={asd} />
+        </a>
         <a href={asd2}>
           <img alt="img1" src={asd} />
         </a>
