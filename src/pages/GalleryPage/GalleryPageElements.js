96db098c2ea858b0a6a3385cd98d334921f6b652
Andras E <rohadtsajt@gmail.com>
2024-07-08 21:53:30 +0100

    styling lightGallery.js

--- src/pages/GalleryPage/GalleryPageElements.js
+++ src/pages/GalleryPage/GalleryPageElements.js
@@ -1,1 +1,24 @@
 import styled from "styled-components";
+
+export const GalleryContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 80%;
+  margin-right: auto;
+  margin-left: auto;
+  margin-top: 80px;
+  justify-content: center;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+export const Img = styled.img`
+  margin: 30px 20px;
+  border-radius: 10px;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
+  @media screen and (max-width: 768px) {
+    margin: 15px 15px;
+  }
+`;
