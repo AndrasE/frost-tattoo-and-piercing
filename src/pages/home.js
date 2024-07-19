14674f5ed84f364be0710598811cdabd18e3f04f
Andras E <rohadtsajt@gmail.com>
2024-07-19 20:10:06 +0100

    test revert
    concluded that i would just make the code harder to follow and not gaining enough w reusing the section component

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,9 +5,6 @@ import HeroSection from "../components/HeroSection";
 import Services from "../components/ServicesSection";
 import About from "../components/AboutSection";
 import Gallery from "../components/GallerySection";
-import Carousel from "../components/GalleryImgCarousel";
-import Section from "../components/SectionComponent";
-import About2 from "../components/AboutSection2";
 
 const Home = () => {
   const [$isOpen, setIsOpen] = useState(false);
@@ -25,7 +22,6 @@ const Home = () => {
       <About />
       <Services />
       <Gallery />
-      <Section col1={<About2 />} col2={<Carousel />} />
     </>
   );
 };
