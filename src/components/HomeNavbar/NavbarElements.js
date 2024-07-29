e1809697eabac5c45d158e0cb416632f7cd1dcf6
AndrasE <rohadtsajt@gmail.com>
2024-07-29 14:27:09 +0100

    1px navbar issue later change scroll react to be precise

--- src/components/HomeNavbar/NavbarElements.js
+++ src/components/HomeNavbar/NavbarElements.js
@@ -5,7 +5,7 @@ import { Link as LinkScroll } from "react-scroll";
 export const Nav = styled.nav`
   background: #16191d;
   height: 80px;
-  margin-top: -50px;
+  margin-top: -80px;
   display: flex;
   justify-content: center;
   align-items: center;
--- src/components/PagesNavbar/NavbarElements.js
+++ src/components/PagesNavbar/NavbarElements.js
@@ -4,7 +4,7 @@ import { Link as LinkRoute } from "react-router-dom";
 export const Nav = styled.nav`
   background: #16191d;
   height: 80px;
-  margin-top: -50px;
+  margin-top: -80px;
   display: flex;
   justify-content: center;
   align-items: center;
