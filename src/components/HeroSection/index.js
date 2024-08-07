aa9986a548712d7c8f2afafebaf4622412bcc1b0
AndrasE <rohadtsajt@gmail.com>
2024-08-07 14:59:15 +0100

    hero video update

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Video from "../../videos/video.mp4";
+import { motion } from "framer-motion";
 
 import {
   HeroContainer,
@@ -12,21 +13,28 @@ import {
 } from "./HeroElements";
 
 const HeroVideo = () => {
+  const [videoEnded, setVideoEnded] = useState(false);
+
   return (
     <HeroContainer>
       <HeroBg>
-        <VideoBg
-          autoPlay
-          muted
-          src={Video}
-          onEnded={() => console.log("ended")}
-          type="video/mp4"
-        />
+        <motion.div
+          initial={{ opacity: 1 }}
+          animate={{ opacity: videoEnded ? 0 : 1 }}
+          transition={{ duration: 2 }}>
+          <VideoBg
+            autoPlay
+            muted
+            src={Video}
+            onEnded={() => setVideoEnded(true)}
+            type="video/mp4"
+          />
+        </motion.div>
       </HeroBg>
       <HeroContent>
         <HeroH1>Frost </HeroH1>
         <HeroH2>tattoo and piercing</HeroH2>
-        <HeroP>Welcome to our offical website!</HeroP>
+        <HeroP>Welcome to our official website!</HeroP>
       </HeroContent>
     </HeroContainer>
   );
