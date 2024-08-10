462611b01365306366b4993977348103d4695a24
Andras E <rohadtsajt@gmail.com>
2024-08-10 10:53:29 +0100

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Navbar from "../components/HomeNavbar";
 import Sidebar from "../components/Sidebar";
 import HeroSection from "../components/HeroSection";
