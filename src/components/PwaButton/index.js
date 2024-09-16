8f22d2a01d03a38e6eb7abe592344f94edc843f0
Andras E <rohadtsajt@gmail.com>
2024-09-16 18:40:52 +0100

    Update index.js

--- src/components/PwaButton/index.js
+++ src/components/PwaButton/index.js
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from "react";
 
 const PWABtn = () => {
   const [deferredPrompt, setDeferredPrompt] = useState(null); // Store the prompt event
-  const [isPromptSupported, setIsPromptSupported] = useState(false); // State to check if install is supported
 
   useEffect(() => {
     const handleBeforeInstallPrompt = (event) => {
       event.preventDefault(); // Prevent automatic prompt
       setDeferredPrompt(event); // Save the event to trigger it later
-      setIsPromptSupported(true); // Show the button if the event is available
       console.log("beforeinstallprompt event:", event);
     };
 
@@ -43,9 +41,7 @@ const PWABtn = () => {
 
   return (
     <div>
-      {isPromptSupported && (
-        <button onClick={handleInstallClick}>Install PWA</button>
-      )}
+      <button onClick={handleInstallClick}>Install PWA</button>
     </div>
   );
 };
