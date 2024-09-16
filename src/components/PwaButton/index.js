50be20736698799e2476e100dd874dc829c78c38
Andras E <rohadtsajt@gmail.com>
2024-09-16 19:12:38 +0100

    test

--- src/components/PwaButton/index.js
+++ src/components/PwaButton/index.js
@@ -1,37 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import "@khmyznikov/pwa-install";
 
 const PWABtn = () => {
-  const [promptInstall, setPromptInstall] = useState(null);
-
-  useEffect(() => {
-    const handler = (e) => {
-      e.preventDefault();
-      console.log("we are being triggered :D");
-      setPromptInstall(e);
-    };
-    window.addEventListener("beforeinstallprompt", handler);
-
-    return () => window.removeEventListener("transitionend", handler);
-  }, []);
-
-  const onClick = (evt) => {
-    evt.preventDefault();
-    if (!promptInstall) {
-      return;
-    }
-    promptInstall.prompt();
-  };
-
-  return (
-    <button
-      className="link-button"
-      id="setup_button"
-      aria-label="Install app"
-      title="Install app"
-      onClick={onClick}>
-      Install
-    </button>
-  );
+  return <pwa-install></pwa-install>;
 };
 
 export default PWABtn;
