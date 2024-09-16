073cfd462d67576578b4a0044e5e43aaf40ddba5
AndrasE <rohadtsajt@gmail.com>
2024-09-16 20:22:07 +0100

    Update index.js

--- src/components/PwaButton/index.js
+++ src/components/PwaButton/index.js
@@ -1,11 +1,34 @@
-import "@khmyznikov/pwa-install";
+import React, { useState, useEffect } from "react";
 
-const PWABtn = () => {
+function PWABtn() {
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (event) => {
+      event.preventDefault();
+      setDeferredPrompt(event);
+    });
+  }, []);
+
+  const handleInstall = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("User accepted the A2HS prompt");
+        } else {
+          console.log("User dismissed the A2HS prompt");
+        }
+        setDeferredPrompt(null);
+      });
+    }
+  };
+
   return (
     <div>
-      <pwa-install></pwa-install>
+      {deferredPrompt && <button onClick={handleInstall}>Install</button>}
     </div>
   );
-};
+}
 
 export default PWABtn;
