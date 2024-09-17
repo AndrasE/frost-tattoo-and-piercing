71e53d141a1edaf9af28352030fcae335cd512ab
Andras E <rohadtsajt@gmail.com>
2024-09-17 12:39:47 +0100

    x

--- src/components/PwaButton/index.js
+++ src/components/PwaButton/index.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 let deferredPrompt;
 
 function PWABtn() {
-  const [installable, setInstallable] = useState(false);
+  // const [installable, setInstallable] = useState(false);
 
   useEffect(() => {
     window.addEventListener("beforeinstallprompt", (e) => {
@@ -12,7 +12,7 @@ function PWABtn() {
       // Stash the event so it can be triggered later.
       deferredPrompt = e;
       // Update UI notify the user they can install the PWA
-      setInstallable(true);
+      // setInstallable(true);
     });
 
     window.addEventListener("appinstalled", () => {
@@ -23,7 +23,7 @@ function PWABtn() {
 
   const handleInstallClick = (e) => {
     // Hide the app provided install promotion
-    setInstallable(false);
+    // setInstallable(false);
     // Show the install prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
@@ -40,11 +40,10 @@ function PWABtn() {
     <div className="App">
       <header className="App-header">
         <h2>Install Demo</h2>
-        {installable && (
-          <button className="install-button" onClick={handleInstallClick}>
-            INSTALL ME
-          </button>
-        )}
+
+        <button className="install-button" onClick={handleInstallClick}>
+          INSTALL ME
+        </button>
       </header>
     </div>
   );
