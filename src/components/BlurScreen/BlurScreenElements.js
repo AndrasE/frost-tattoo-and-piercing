d66ad969573a05535577d125d5f8aa542a039fd5
AndrasE <rohadtsajt@gmail.com>
2024-09-20 18:15:38 +0100

    Update BlurScreenElements.js

--- src/components/BlurScreen/BlurScreenElements.js
+++ src/components/BlurScreen/BlurScreenElements.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const BlurAnimation = styled.div`
   position: absolute;
   width: 100%;
-  height: 100vh;
+  height: 150vh;
   backdrop-filter: blur(6px);
   z-index: ${({ $settingsOpen }) => ($settingsOpen ? 6 : 0)};
   transition: filter 0.3s ease-in-out, background-color 0.3s ease-in-out;
