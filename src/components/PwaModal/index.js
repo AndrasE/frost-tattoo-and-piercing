5d0c554e0af88841d167f2b2a8ea02cb3bd6bfef
AndrasE <rohadtsajt@gmail.com>
2024-09-21 20:27:33 +0100

    conditionally render pwa modal

--- src/components/PwaModal/index.js
+++ src/components/PwaModal/index.js
@@ -13,25 +13,54 @@ import {
   Path,
 } from "./PwaModalElements";
 import logo from "../../images/logoImgs/logo.png";
-// import SafariDesktop from "./SafariDesktop";
-// import SafariMobile from "./SafariMobile";
-// import ChromiumsInstallAvail from "./ChromiumsInstallAvail";
-// import ChromiumsInstalledAlready from "./ChromiumsInstalledAlready";
-// import FirefoxOperaMobile from "./FirefoxOperaMobile";
+import ChromiumsInstallAvail from "./ChromiumsInstallAvail";
+import ChromiumsInstalledAlready from "./ChromiumsInstalledAlready";
+import SafariMobile from "./SafariMobile";
+import SafariDesktop from "./SafariDesktop";
+import FirefoxOperaMobile from "./FirefoxOperaMobile";
 import FirefoxDesktop from "./FirefoxDesktop";
 
 const PwaModal = ({
   $pwaModalOpen,
   togglePwaModal,
-  // browserSettings,
+  browserSettings,
   deferredPrompt,
 }) => {
+  // Conditional rendering according to browiser and OS
+  const renderPwaModal = () => {
+    const { name, os } = browserSettings;
+
+    // Logic for Edge and Chrome
+    if ((name === "chrome" || name === "edge") && deferredPrompt) {
+      return <ChromiumsInstallAvail deferredPrompt={deferredPrompt} />;
+    } else if (name === "chrome" || name === "edge") {
+      return <ChromiumsInstalledAlready />;
+    }
+
+    // Logic for Safari
+    if (name === "safari" && os === "macOS") {
+      return <SafariDesktop />;
+    } else if (name === "safari" && os === "iOS") {
+      return <SafariMobile />;
+    }
+
+    // Logic for Firefox
+    if (name === "firefox" && os === "windows") {
+      return <FirefoxDesktop />;
+    } else if ((name === "firefox" || name === "opera") && os === "android") {
+      return <FirefoxOperaMobile />;
+    }
+
+    // Fallback for unsupported browsers/OS
+    return <h1>Browser or OS not supported</h1>;
+  };
+
   return (
     <MotionModal
       $sidebarOpen={$pwaModalOpen}
       onClick={togglePwaModal}
       initial={{
-        bottom: "-100%", // Set initial top position
+        bottom: "-100%", // Set initial position
       }}
       animate={{
         bottom: $pwaModalOpen ? "0" : "-100%",
@@ -47,7 +76,7 @@ const PwaModal = ({
               <HeaderAddress>{window.location.href}</HeaderAddress>
             </HeaderTitleWrapper>
           </HeaderTitleRow>
-          <HeaderBtn>
+          <HeaderBtn onClick={togglePwaModal}>
             <Svg>
               <Path d="M 3 16.5 L 17 2.5"></Path>
               <Path d="M 3 2.5 L 17 16.346"></Path>
@@ -55,12 +84,8 @@ const PwaModal = ({
           </HeaderBtn>
         </HeaderContainer>
 
-        {/* <SafariMobile /> */}
-        {/* <ChromiumsInstallAvail deferredPrompt={deferredPrompt} />
-        <ChromiumsInstalledAlready /> */}
-        {/* <SafariDesktop /> */}
-        {/* <FirefoxOperaMobile /> */}
-        <FirefoxDesktop />
+        {/* Conditionally render the appropriate content */}
+        {renderPwaModal()}
       </ModalContainer>
     </MotionModal>
   );
